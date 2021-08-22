8f06467d95351a44a7bc7b3cfd08b4d77f2ed738
Xyzzy Apps <xyzzyapps@gmail.com>
2021-08-22 17:38:27 +0100

    Fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Submission for langjam#0001
 
-see [https://xyzzyapps.github.com/loom].
+see https://xyzzyapps.github.io/loom for the docs and setup.
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@ copyright = '2021, Xyzzy'
 author = 'Xyzzy'
 release = '2021'
 extensions = ['myst_parser', 'sphinx.ext.todo']
+html_baseurl = "https://xyzzyapps.github.io/loom/"
 
 templates_path = ['_templates']
 exclude_patterns = []
