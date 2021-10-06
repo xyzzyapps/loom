b160b3f641df6b0a2d6445211c82afa6a8455ed3
Xyzzy Apps <xyzzyapps@gmail.com>
2021-10-06 01:46:54 +0100

    Separating Jam code

--- source/jam.py
+++ source/jam.py
@@ -1,3 +1,8 @@
+# animation::quine:1
+# Welcome!
+
+# imports
+
 from watchpoints import watch
 from flexicon import Lexer
 import tempfile
@@ -9,6 +14,64 @@ import os
 import sqlite3
 from sqlite3 import Error
 
+# end::animation
+
+
+conn = None
+
+# animation::quine:2
+
+# basic sql
+
+def execute(conn, query, args):
+    cur = conn.cursor()
+    cur.execute(query, args)
+    conn.commit()
+    return cur.lastrowid
+
+# end::animation
+
+def select_query(conn, query, params, mapper):
+    cur = conn.cursor()
+    if params:
+        cur.execute(query, params)
+    else:
+        cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        mapper(row)
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+sql_create_codebase_table = """
+CREATE TABLE IF NOT EXISTS codebase (
+    file_path text NOT NULL,
+    array_index integer NOT NULL,
+    type text NOT NULL,
+    line text NOT NULL
+);
+"""
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
 def dir_walk(dir, mapper):
     for root, dirs, files in os.walk(dir):
         for file in files:
@@ -19,6 +82,7 @@ def ends_with_slash(dir_name):
         return dir_name
     else:
         return dir_name + "/"
+
 
 def output_printer(x):
     print(x, end="")
@@ -50,6 +114,71 @@ def string_state():
         final_str += x
 
     return [getter, setter]
+
+def parse(st, print_wrapper, code_wrapper, with_fence=True):
+    lexer = Lexer().simple(
+        (r'(\n[\n ]*)', lambda _1: ('NL', _1)),
+        (r'^#(.+\n)', lambda _1: ('COMMENT', _1)),
+        (r'(\s*)(.+?)#(.+\n)', lambda _1, _2, _3: ('PARTIAL_COMMENT', _1, _2, _3)),
+        (r'^(\s*)(.+\n)', lambda _1, _2: ('LINE', _1, _2)),
+    )
+
+    tokens = lexer.lex(st)
+    stack = []
+
+    line_count = 1
+    in_middle = []
+    is_program_beginning = True
+
+    comment_sequences = []
+    code_sequences = []
+
+    current_code_sequence = ""
+    current_comment_sequence = ""
+    sequence_index = 0
+
+    for token in tokens:
+        if line_count > 1:
+            is_program_beginning = False
+
+        if token[0] == "COMMENT":
+            current_comment_sequence += token[1]
+            line_count += 1
+        if token[0] == "PARTIAL_COMMENT":
+            if len(token[2]) > 0:
+                current_code_sequence += token[1]
+            current_code_sequence += token[2]
+            current_comment_sequence += token[3]
+            line_count += 1
+        if token[0] == "LINE":
+            current_code_sequence += token[1] 
+            current_code_sequence += token[2] + "\n"
+            if len(token[1]) == 0:
+                if current_code_sequence != "" and current_comment_sequence != "":
+                    code_sequences.append(current_code_sequence)
+                    comment_sequences.append(current_comment_sequence)
+                    sequence_index += 1
+                    current_code_sequence = ""
+                    current_comment_sequence = ""
+
+            line_count += 1
+        else:
+            line_count += 1
+
+    if current_code_sequence != "" and current_comment_sequence != "":
+        code_sequences.append(current_code_sequence)
+        comment_sequences.append(current_comment_sequence)
+
+    for comment in comment_sequences:
+        print_wrapper(comment)
+    if with_fence:
+        code_wrapper("\n```")
+    for code in code_sequences:
+        code_wrapper(code)
+    if with_fence:
+        print_wrapper("\n```")
+
+    return [code_sequences, comment_sequences]
 
 def parse_array(st):
     lexer = Lexer().simple(
@@ -124,6 +253,51 @@ def parse_array(st):
 if __name__ == "__main__":
 
     arg1 = sys.argv[1]
+
+    if arg1 == "--clean-source":
+        parse(sys.stdin.read(), null_printer, output_printer, with_fence=False)
+
+    if arg1 == "--generate-docs":
+
+        os.system("safe-rm codedb.db")
+        conn = create_connection("codedb.db")
+        create_table(conn, sql_create_codebase_table)
+
+        arg2 = sys.argv[2]
+        output_dir = ends_with_slash(sys.argv[3])
+
+        def run_parser(file_name):
+            global conn
+            file_contents = open(file_name).read()
+            file_printer = file_printer_prototype(output_dir, file_name)
+            sequences = parse(file_contents, file_printer, file_printer)
+            sql = ''' INSERT INTO codebase(file_path, array_index, type, line)
+                VALUES(?,?,?,?) '''
+            for i, e in enumerate(sequences[0]):
+                execute(conn, sql, [file_name, i, "code", e])
+            for i, e in enumerate(sequences[1]):
+                execute(conn, sql, [file_name, i, "comment", e])
+
+
+        dir_walk(arg2, run_parser)
+
+    if arg1 == "--generate-index":
+
+        arg2 = sys.argv[2]
+
+        print("""Welcome
+=======
+
+.. toctree::
+    :caption: Table of Contents
+    :maxdepth: 5
+    :glob:
+    :includehidden:
+
+""")
+
+        def run_parser(file_name):
+            print("    " + re.sub(arg2 + "/", "", file_name))
 
     if arg1 == "--generate-terminal-show":
 
@@ -208,4 +382,24 @@ if __name__ == "__main__":
                 os.system("cowsay -W 72 <" + temp.name)
                 time.sleep(slide_delay)
 
+    if arg1 == "--comment-stats":
+
+        comments_count = 0
+        code_count = 0
+
+        def comment_counter(row):
+            global comments_count
+            comments_count += len(row[3].split("\n"))
+
+        def code_counter(row):
+            global code_count
+            code_count += len(row[3].split("\n"))
+
+        conn = create_connection("codedb.db")
+        sql = """ SELECT * FROM codebase WHERE type=? """
+        select_query(conn, sql, ["comment"], comment_counter)
+        select_query(conn, sql, ["code"], code_counter)
+        print("comments loc: " + str(comments_count))
+        print("code loc: " + str(code_count))
+        print("ratio (higher is better): " + str(comments_count / code_count))
 
