9b27f735c02decb6b141afc969078551be0487f5
Xyzzy Apps <xyzzyapps@gmail.com>
2021-10-19 20:03:08 +0100

    Analytics

--- _static/analytics.js
+++ _static/analytics.js
@@ -1,3 +1,4 @@
-window.goatcounter = {
-    path: function(p) { return location.host + p }
-}
+var _paq = window._paq = window._paq || [];
+_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);_paq.push(['alwaysUseSendBeacon']);_paq.push(['setTrackerUrl', "\/\/blog.xyzzyapps.link\/owoamims\/matomo\/app\/matomo.php"]);_paq.push(['setSiteId', '1']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.src="\/\/blog.xyzzyapps.link\/oozotche\/matomo\/matomo.js"; s.parentNode.insertBefore(g,s);
+
